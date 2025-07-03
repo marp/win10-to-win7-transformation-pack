3bb2d70515550498bb2dcc50d2da48e40fd36d30
marp <mapp0@protonmail.com>
2025-07-03 20:17:16 +0200

    fix

--- CPL Restoration 4.0 H1/GenuineCenter.ps1
+++ CPL Restoration 4.0 H1/GenuineCenter.ps1
@@ -1,5 +1,8 @@
 # You have to use this together with the 8.X styled System CPL located at "CPL Restoration\Pages\System CPL\8.X Style" to make it appear correctly.
 
+# Ensure script runs in its own directory
+Set-Location -Path (Split-Path -Parent $PSCommandPath)
+
 #Import the .reg files located at "CPL Restoration\Pages\Genuine Center CPL\Import as TrustedInstaller" as TrustedInstaller.
 Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList 'reg import "Pages\Genuine Center CPL\Import as TrustedInstaller\genuine.reg"' -WindowStyle Hidden -Wait
 
@@ -11,7 +14,7 @@ Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList 'reg import "Pages\
 #\ActionCenterCPL.dll.mun" into "ActionCenterCPL.dll.mun" 
 #file located at "C:\Windows\SystemResources".
 #Choose "overwrite" if prompted.
-Start-Process ".\..\resource_hacker\ResourceHacker" -ArgumentList '-open C:\Windows\SystemResources\ActionCenterCPL.dll.mun', '-resource "C:\Users\Marcin\Downloads\win10-to-win7-transformation-pack\CPL Restoration 4.0 H1\Pages\Genuine Center CPL\systemresources\ActionCenterCPL.dll.mun\genuinepage.res"', '-save C:\Windows\SystemResources\ActionCenterCPL.dll.mun', '-action addoverwrite'
+Start-Process ".\..\resource_hacker\ResourceHacker" -ArgumentList '-open C:\Windows\SystemResources\ActionCenterCPL.dll.mun', '-resource "Pages\Genuine Center CPL\systemresources\ActionCenterCPL.dll.mun\genuinepage.res"', '-save C:\Windows\SystemResources\ActionCenterCPL.dll.mun', '-action addoverwrite'
 
 # Use Resource Hacker to import the included files from 
 # "CPL Restoration\Pages\Genuine Center CPL\system32\en-US
--- Themes/copy.ps1
+++ Themes/copy.ps1
@@ -9,7 +9,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 # Ensure script runs in its own directory
 Set-Location -Path (Split-Path -Parent $PSCommandPath)
 
-Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList "powershell -ExecutionPolicy Bypass -Command Copy-Item -Path '.\' -Destination 'C:\Windows\Resources\Themes\' -Recurse -Force" -Wait -WindowStyle Hidden
+Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList "powershell -ExecutionPolicy Bypass -Command Copy-Item -Path '.\*' -Destination 'C:\Windows\Resources\Themes\' -Recurse -Force" -Wait -WindowStyle Hidden
 Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList "powershell -ExecutionPolicy Bypass -Command Remove-Item -Path 'C:\Windows\Resources\Themes\copy.ps1' -Force" -Wait -WindowStyle Hidden
 
 Write-Host "Files copied successfully!" -ForegroundColor Green
