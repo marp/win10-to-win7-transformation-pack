fb35b7dc75d4c090b34470273442428f4462c7f2
marp <mapp0@protonmail.com>
2025-07-10 15:36:02 +0200

    Windows Cardspace

--- CPL Restoration 4.0 H1/WindowsCardspace.ps1
+++ CPL Restoration 4.0 H1/WindowsCardspace.ps1
@@ -2,4 +2,6 @@ Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList 'reg import "Pages\
 
 Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList "powershell -ExecutionPolicy Bypass -Command Copy-Item -Path '.\Pages\Windows Cardspace CPL\7 Style\system32\*' -Destination 'C:\Windows\System32' -Recurse -Force" -Wait -WindowStyle Hidden
 Start-Process ".\..\PowerRun\PowerRun_x64.exe" -ArgumentList "powershell -ExecutionPolicy Bypass -Command Copy-Item -Path '.\Pages\Windows Cardspace CPL\7 Style\Microsoft.NET\*' -Destination 'C:\Windows\Microsoft.NET' -Recurse -Force" -Wait -WindowStyle Hidden
-# Set-Service '' -StartupType Automatic
+
+Write-Warning "A system restart is required to enable the Windows Cardspace service. Please restart your computer and then run this script again to complete the installation."
+Set-Service 'idsvc' -StartupType Automatic
